98a4fbdce32cea04dee99fceac006ef59e643c81
Nathan Blair <nblair@berkeley.edu>
2017-08-21 17:34:43 -0700

    small change

--- assets/PHP/includes/constants.php
+++ assets/PHP/includes/constants.php
@@ -4,5 +4,5 @@
 
 define('DB_SERVER', 'localhost');
 define('DB_USER', 'nathanbl_nathan');
-define('DB_PASSWORD', 'Nathan978!');
+define('DB_PASSWORD', '123nate!45');
 define('DB_NAME', 'nathanbl_GOL_HighScore');
